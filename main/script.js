cde6bd0d477ab488b802d5c68c8e01d2cfafb144
gamlinskijoleg <gamlinskijoleg@gmail.com>
2024-12-07 17:49:49 +0200

    new commit

--- main/script.js
+++ main/script.js
@@ -20,13 +20,13 @@ const recommendations = document.getElementById("recomendations");
 const popupBox = document.getElementById("popup-box");
 
 let currentVolume = 10;
-var audio1 = new Audio("/misc/sounds/sound1.mp3");
+let audio1 = new Audio("/misc/sounds/sound1.mp3");
 let audio2 = new Audio("/misc/sounds/sound2.mp3");
 let audio3 = new Audio("/misc/sounds/sound3.mp3");
 let audio4 = new Audio("/misc/sounds/sound4.mp3");
 let currentVolumeMenuOpen = false;
 
-var IsDoNotShowAgain = localStorage.getItem("IsDoNotShowAgain") === "true";
+let IsDoNotShowAgain = localStorage.getItem("IsDoNotShowAgain") === "true";
 checkbox.checked = IsDoNotShowAgain;
 
 const allAudios = [audio1, audio2, audio3, audio4];
